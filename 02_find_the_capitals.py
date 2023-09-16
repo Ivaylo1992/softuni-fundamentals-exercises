ca41d8aaab46da8ee17129a2e00f031c5b175fad
Ivaylo1992 <ivaylop123@gmail.com>
2023-09-16 22:15:33 +0300

    find the capitals

---
+++ 02_find_the_capitals.py
@@ -0,0 +1,13 @@
+input_line = input()
+
+#empty list to store the indexes in it
+my_list = []
+
+#loop over the string and find the capital letters
+for i in range(len(input_line)):
+    if input_line[i].isupper():
+        my_list.append(i)
+
+# print the result
+print(my_list)
+
