3d042b0009fec3fab56ad863639d2bef1c0a7892
Ivaylo1992 <ivaylop123@gmail.com>
2023-09-20 05:47:54 +0300

    computer store challenge

---
+++ 01_computer_store.py
@@ -0,0 +1,34 @@
+total_price = 0
+
+while True:
+    input_line = input()
+
+    if input_line.isalpha():
+        taxes = total_price * 0.2
+        total_price_with_taxes = taxes + total_price
+
+        if total_price == 0:
+            print('Invalid order!')
+            break
+
+        if input_line == 'special':
+            discount_price = total_price_with_taxes * 0.9
+
+        break
+
+    price = float(input_line)
+    if price < 0:
+        print("Invalid price!")
+        continue
+
+    total_price += price
+
+if total_price > 0:
+    print("Congratulations you've just bought a new computer!")
+    print(f'Price without taxes: {total_price:.2f}$')
+    print(f'Taxes: {taxes:.2f}$')
+    print('-----------')
+    if input_line == 'special':
+        print(f'Total price: {discount_price:.2f}$')
+    else:
+        print(f'Total price: {total_price_with_taxes:.2f}$')
