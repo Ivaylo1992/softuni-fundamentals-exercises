ab5257af3778ff5a73277550842d7913981bdc58
Ivaylo1992 <ivaylop123@gmail.com>
2023-09-21 06:11:30 +0300

    counter strike challenge

---
+++ 01_counter_strike.py
@@ -0,0 +1,21 @@
+energy = int(input())
+counter = 0
+while True:
+    command = input()
+
+    if command == 'End of battle':
+        print(f"Won battles: {counter}. Energy left: {energy}")
+        break
+
+    enemy_distance = int(command)
+
+    if enemy_distance > energy:
+        print(f"Not enough energy! Game ends with {counter} won battles and {energy} energy")
+        break
+
+    energy -= enemy_distance
+    counter += 1
+
+    if counter % 3 == 0:
+        energy += counter
+
