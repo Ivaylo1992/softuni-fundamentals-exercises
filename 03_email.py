23654d09ff1a528c9cb8925d8d2c2485a37cda8f
Ivaylo Petkov <142958598+Ivaylo1992@users.noreply.github.com>
2023-12-04 05:27:33 +0200

    03_email.py

--- 03_email.py
+++ 03_email.py
@@ -9,7 +9,7 @@ class Email:
 
     def send(self):
         self.is_sent = True
-
+        
     def get_info(self):
         return f"{self.sender} says to {self.receiver}: {self.content}. Sent: {self.is_sent}"
 
