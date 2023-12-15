5ac17744783aa18b1283b57ec081dccdd3f79ef2
Ivaylo Petkov <142958598+Ivaylo1992@users.noreply.github.com>
2023-12-15 05:33:20 +0200

    01_find_the_largest.py

--- 01_find_the_largest.py
+++ 01_find_the_largest.py
@@ -1,13 +1,13 @@
 number = int(input())
 
-#creating a list from the charecters of the string of our  input
+# Create a list from the charecters of the string of our  input
 lst_num = [int(char) for char in str(number)]
 
-#sorting the numbers from the largest to the smallest one
+# Sort the numbers from the largest to the smallest one
 max_num_lst = sorted(lst_num, reverse=True)
 
-#making a list of strings from the numbers in order to join them after in a single string
+# Make a list of strings from the numbers in order to join them after in a single string
 max_num_str = [str(num) for num in max_num_lst]
 
-# print the result
+# Print the result
 print((int(''.join(max_num_str))))
